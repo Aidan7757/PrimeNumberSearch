05fc1f6dd40475a222e5b5842d9fc2520f603cc2
Aidan Chadha <aidanchadha03@vt.edu>
2026-01-15 17:56:28 -0500

    test change

--- tests/search_methods_openmp_test.c
+++ tests/search_methods_openmp_test.c
@@ -3,8 +3,9 @@
 #include <omp.h>
 #include "../src/search_methods_openmp.h"
 #include "../src/models.h"
+#include "../src/bn.h"
 
-void general_prime_cpu_test(bool (*func)(__int128_t prime_to_check, Config* config),
+void general_prime_cpu_test(bool (*func)(BigNum* prime_to_check, Config* config),
                             Config* config) {
     long long prime_count = 0;
     const double start_time = omp_get_wtime();
@@ -16,12 +17,27 @@ void general_prime_cpu_test(bool (*func)(__int128_t prime_to_check, Config* conf
         printf("========================================\n\n");
     }
 
-    #pragma omp parallel for num_threads(config->num_threads) \
-            reduction(+:prime_count) schedule(dynamic, 1000)
-    for (__int128_t i = config->lower_range; i < config->max_range; ++i) {
-        const bool prime_result = func(i, config);
-        if (prime_result) {
-            prime_count++;
+    // Calculate range size for iteration
+    BigNum range_size;
+    bn_sub(&range_size, &config->max_range, &config->lower_range);
+    __int128_t iterations = bn_to_int128(&range_size);
+
+    #pragma omp parallel num_threads(config->num_threads) reduction(+:prime_count)
+    {
+        BigNum current;
+        bn_init(&current);
+
+        #pragma omp for schedule(dynamic, 1000)
+        for (__int128_t i = 0; i < iterations; ++i) {
+            // current = lower_range + i
+            BigNum offset;
+            bn_from_int128(&offset, i);
+            bn_add(&current, &config->lower_range, &offset);
+
+            const bool prime_result = func(&current, config);
+            if (prime_result) {
+                prime_count++;
+            }
         }
     }
 
@@ -30,15 +46,14 @@ void general_prime_cpu_test(bool (*func)(__int128_t prime_to_check, Config* conf
 
     printf("Method: %s\n", config_get_mul_method_name(config->mul_method));
     printf("Time:   %.6f seconds\n", overall_cpu_time);
-    printf("Range:  %lld iterations\n",
-           (long long)(config->max_range - config->lower_range));
+    printf("Range:  %lld iterations\n", (long long)iterations);
     printf("Primes: %lld found\n", prime_count);
     printf("Speed:  %.2f checks/sec\n\n",
-           (double)(config->max_range - config->lower_range) / overall_cpu_time);
+           (double)iterations / overall_cpu_time);
 }
 
 int main() {
-    typedef bool (*f) (__int128_t, Config*);
+    typedef bool (*f) (BigNum*, Config*);
     f functions[] = {
         (f)&miller_rabin,
         (f)&fermat,
@@ -47,44 +62,51 @@ int main() {
     };
     const char* func_names[] = {"Miller-Rabin", "Fermat", "Gauss-Euler", "MR-GE"};
 
-    // Test configurations
-    Config configs[5];
+    // Test configurations - all using 2048 bits
+    const int BIT_SIZE = 2048;
+    Config configs[3];
 
-    // Config 0: Native __int128_t (baseline)
+    // Generate a single random starting point
+    BigNum base_range;
+    bn_random(&base_range, BIT_SIZE);
+
+    BigNum offset;
+    bn_from_int128(&offset, 1000000);
+
+    // Config 0: BigNum Standard
     config_init(&configs[0]);
-    configs[0].mul_method = MUL_NATIVE_INT128;
-    configs[0].use_bignum = false;
+    bn_copy(&configs[0].lower_range, &base_range);
+    bn_add(&configs[0].max_range, &base_range, &offset);
+    configs[0].mul_method = MUL_BIGNUM_STANDARD;
+    configs[0].use_bignum = true;
+    configs[0].bignum_bit_size = BIT_SIZE;
 
-    // Config 1: BigNum Standard
+    // Config 1: BigNum Karatsuba
     config_init(&configs[1]);
-    configs[1].mul_method = MUL_BIGNUM_STANDARD;
+    bn_copy(&configs[1].lower_range, &base_range);
+    bn_add(&configs[1].max_range, &base_range, &offset);
+    configs[1].mul_method = MUL_BIGNUM_KARATSUBA;
     configs[1].use_bignum = true;
+    configs[1].bignum_bit_size = BIT_SIZE;
 
-    // Config 2: BigNum Karatsuba
+    // Config 2: BigNum Schönhage-Strassen
     config_init(&configs[2]);
-    configs[2].mul_method = MUL_BIGNUM_KARATSUBA;
+    bn_copy(&configs[2].lower_range, &base_range);
+    bn_add(&configs[2].max_range, &base_range, &offset);
+    configs[2].mul_method = MUL_BIGNUM_SS;
     configs[2].use_bignum = true;
-
-    // Config 3: BigNum Schönhage-Strassen
-    config_init(&configs[3]);
-    configs[3].mul_method = MUL_BIGNUM_SS;
-    configs[3].use_bignum = true;
-
-    // Config 4: Auto-select (with verification)
-    config_init(&configs[4]);
-    configs[4].mul_method = MUL_AUTO;
-    configs[4].use_bignum = true;
-    configs[4].verify_results = true;  // Verify against native
+    configs[2].bignum_bit_size = BIT_SIZE;
 
     printf("========================================\n");
     printf("  PRIME SEARCH BENCHMARK\n");
+    printf("  Bit Size: %d\n", BIT_SIZE);
     printf("========================================\n");
 
     // Test each primality function with each config
     for (size_t func_idx = 0; func_idx < sizeof(functions) / sizeof(functions[0]); ++func_idx) {
         printf("\n\n=== Testing: %s ===\n", func_names[func_idx]);
 
-        for (size_t cfg_idx = 0; cfg_idx < 5; ++cfg_idx) {
+        for (size_t cfg_idx = 0; cfg_idx < 3; ++cfg_idx) {
             general_prime_cpu_test(functions[func_idx], &configs[cfg_idx]);
         }
     }
